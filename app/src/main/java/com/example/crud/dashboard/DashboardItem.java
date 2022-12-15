56dec30ee675043bc9a31a2ad256872caaf4823e
Suresh <suresh.sanganaboyana@improve10x.com>
2022-12-15 11:32:44 +0530

    540 String name i gave for it title only

--- app/src/main/java/com/example/crud/dashboard/DashboardItem.java
+++ app/src/main/java/com/example/crud/dashboard/DashboardItem.java
@@ -2,6 +2,5 @@ package com.example.crud.dashboard;
 
 public class DashboardItem {
     public String imageUrl;
-    //TODO : String name can give only title
-    public String titleText;
+    public String title;
 }
