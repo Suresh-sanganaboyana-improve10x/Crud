c9ae82da04fe01429c607c894ebdced2003da8f6
Suresh <suresh.sanganaboyana@improve10x.com>
2022-12-15 15:18:03 +0530

    546 created constructor for DashboardItem

--- app/src/main/java/com/example/crud/dashboard/DashboardItemsActivity.java
+++ app/src/main/java/com/example/crud/dashboard/DashboardItemsActivity.java
@@ -12,9 +12,8 @@ import com.example.crud.R;
 
 import java.util.ArrayList;
 
-public class DashboardActivity extends AppCompatActivity {
+public class DashboardItemsActivity extends AppCompatActivity {
 
-    private ProgressBar progressBar;
     private ArrayList<DashboardItem> dashboardItems;
     private RecyclerView dashboardItemsRv;
     private DashboardAdapter dashboardItemsAdapter;
@@ -41,31 +40,17 @@ public class DashboardActivity extends AppCompatActivity {
     }
 
     private void setupData() {
-        hideProgressBar();
-        //TODO : create constructor for this
         dashboardItems = new ArrayList<>();
-        DashboardItem message = new DashboardItem();
-        message.imageUrl = "https://www.techadvisor.com/wp-content/uploads/2022/06/how-to-read-facebook-messages-without-them-knowing-main.jpg?quality=50&strip=all&w=1024";
-        message.title = "Message";
+        DashboardItem message = new DashboardItem("Message", "https://www.techadvisor.com/wp-content/uploads/2022/06/how-to-read-facebook-messages-without-them-knowing-main.jpg?quality=50&strip=all&w=1024");
         dashboardItems.add(message);
 
-        DashboardItem templates = new DashboardItem();
-        templates.imageUrl = "https://mixkit.imgix.net/static/home/free-video-templates/free-final-cut-pro-templates.png?q=80&auto=format%2Ccompress";
-        templates.title = "Templates";
+        DashboardItem templates = new DashboardItem("Templates", "https://mixkit.imgix.net/static/home/free-video-templates/free-final-cut-pro-templates.png?q=80&auto=format%2Ccompress");
         dashboardItems.add(templates);
 
-        DashboardItem series = new DashboardItem();
-        series.imageUrl = "https://www.sleepspa.in/wp-content/uploads/2020/04/money-heist-netflix.jpeg";
-        series.title = "Series";
+        DashboardItem series = new DashboardItem("Series", "https://www.sleepspa.in/wp-content/uploads/2020/04/money-heist-netflix.jpeg");
         dashboardItems.add(series);
 
-        DashboardItem movies = new DashboardItem();
-        movies.imageUrl = "https://collegerealitycheck.com/wp-content/uploads/film-studies-degree-374366191-1024x768.jpg";
-        movies.title = "Movies";
+        DashboardItem movies = new DashboardItem("Movies", "https://collegerealitycheck.com/wp-content/uploads/film-studies-degree-374366191-1024x768.jpg");
         dashboardItems.add(movies);
-    }
-
-    private void hideProgressBar() {
-        progressBar.setVisibility(View.GONE);
     }
 }
